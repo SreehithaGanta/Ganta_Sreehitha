0cd73fb304b83c31ab0166b61afcb5083a6e39c3
SreehithaGanta <166277327+SreehithaGanta@users.noreply.github.com>
2024-12-17 19:12:03 +0530

    Add files via upload
    The Handwritten Digit Recognition project classifies digits (0–9) using Support Vector Classification (SVC). A custom dataset was created, and images were preprocessed for consistency. The SVC model was trained and evaluated, achieving high accuracy. Applications include postal sorting and check processing, with future scope for deep learning enhancement

---
+++ Handwritten digit recognition1.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pyscreenshot as ImageGrab
+import time
+
+
+# # creating dataset
+
+# In[4]:
+
+
+images_folder="E:/folder/captured_images/9/"
+  
+for i in range(0,100):
+    time.sleep(8)
+    im=ImageGrab.grab(bbox=(221,221,801,801)) 
+    print("saved......",i)
+    im.save(images_folder+str(i)+'.png')
+    print("clear screen now and redraw now........")
+
+
+# In[13]:
+
+
+pip install opencv-python
+
+
+# In[9]:
+
+
+pip install pandas
+
+
+# In[10]:
+
+
+pip install scikit-learn
+
+
+# # converting the images into zeros and ones
+
+# In[11]:
+
+
+import cv2
+import csv
+import glob
+
+header = ["label"]
+for i in range(0,784):
+    header.append("pixel"+str(i))
+with open('dataset1.csv','a') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    
+for label in range(10):
+    dirList = glob.glob("E:/folder/captured_images/"+str(label)+"/*.png")
+    
+    for img_path in dirList:
+        im= cv2.imread(img_path)
+        im_gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+        im_gray = cv2.GaussianBlur(im_gray,(15,15),0)
+        roi= cv2.resize(im_gray,(28,28), interpolation=cv2.INTER_AREA)
+        
+        data=[]
+        data.append(label)
+        rows, cols = roi.shape
+        for i in range(rows):       #add picxel one by one into data array
+            for j in range(cols):
+                k =roi[i,j]
+                if k>100:
+                    k=1
+                else:
+                    k=0
+                data.append(k)
+        with open('dataset1.csv', 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(data)
+
+        
+
+
+# # load the dataset
+
+# In[3]:
+
+
+import pandas as pd #pip install pandas
+from sklearn.utils import shuffle #pip install scikit-learn
+#0,....,1.....,2.....
+#5,3,1,0,2,5,.......
+ 
+data = pd.read_csv('dataset1.csv')
+data = shuffle(data)
+print(data)
+
+
+# In[4]:
+
+
+X = data.drop(["label"],axis=1)
+Y= data["label"]
+
+
+# In[15]:
+
+
+pip install matplotlib
+
+
+# In[5]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib.pyplot as plt
+import cv2
+idx = 314
+img = X.loc[idx].values.reshape(28,28)
+print(Y[idx])
+plt.imshow(img)
+
+
+# In[11]:
+
+
+from sklearn.model_selection import train_test_split
+train_x,test_x,train_y,test_y = train_test_split(X,Y, test_size = 0.2)
+
+
+# # fit the model using svc and also save the model using joblib
+
+# In[14]:
+
+
+import joblib
+from sklearn.svm import SVC
+classifier=SVC(kernel="linear", random_state=6)
+classifier.fit(train_x,train_y)
+joblib.dump(classifier, "E:/folder/model/digit_recognizer")
+
+
+# # calculating accuracy
+
+# In[ ]:
+
+
+from sklearn import metrics
+prediction=classifier.predict(test_x)
+print("Accuracy= ",metrics.accuracy_score(prediction, test_y))
+
+
+# # prediction of image drawn in paint
+
+# In[ ]:
+
+
+import joblib
+import cv2
+import numpy as np #pip install numpy
+import time
+import pyscreenshot as ImageGrab
+  
+model=joblib.load("E:/folder/model/digit_recognizer")
+images_folder="E:/folder/img/"
+  
+while True:
+    img=ImageGrab.grab(bbox=(221,221,801,801))
+    
+    
+    img.save(images_folder+"img.png")
+    im = cv2.imread(images_folder+"img.png")
+    im_gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    im_gray  =cv2.GaussianBlur(im_gray, (15,15), 0)
+    
+    #Threshold the image
+    ret, im_th = cv2.threshold(im_gray,100, 255, cv2.THRESH_BINARY)
+    roi = cv2.resize(im_th, (28,28), interpolation  =cv2.INTER_AREA)
+    
+    rows,cols=roi.shape
+    
+    X = []
+    
+    ##  Fill the data array with pixels one by one.
+    for i in range(rows):
+        for j in range(cols):
+            k = roi[i,j]
+            if k>100:
+                k=1
+            else:
+                k=0
+            X.append(k)
+            
+    predictions  =model.predict([X])
+
+    print("Prediction:",predictions[0])
+    cv2.putText(im, "Prediction is: "+str(predictions[0]), (20,20), 0, 0.8,(0,255,0),2,cv2.LINE_AA)
+    
+    cv2.startWindowThread()
+    cv2.namedWindow("Result")
+    cv2.imshow("Result",im)
+    cv2.waitKey(10000)
+    if cv2.waitKey(1)==13: #13 is the ascii value of esc, 13 is the ascii value of enter
+        break
+cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
